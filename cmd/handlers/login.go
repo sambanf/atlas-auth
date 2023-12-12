ccf78630c13717e814017b7fabe1da187dd8c7b6
Samba Nugraha <sambanugraha@gmail.com>
2023-12-12 07:52:51 +0700

    Add Login Handlers

---
+++ cmd/handlers/login.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Login struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func HandleLogin(c *gin.Context) {
+	var login Login
+	if err := c.ShouldBindJSON(&login); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if login.Username != "admin" || login.Password != "admin" {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
+	"atlas-auth/cmd/handlers" // adjust this import path to match your project structure
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-
-type Login struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
 
 func main() {
 	r := gin.Default()
@@ -18,21 +14,7 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"response": "hello world"})
 	})
 
-	r.POST("/login", func(c *gin.Context) {
-		var login Login
-
-		if err := c.ShouldBindJSON(&login); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		if login.Username != "admin" || login.Password != "admin" {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
-	})
+	r.POST("/login", handlers.Login)
 
 	r.Run(":8080")
 }
